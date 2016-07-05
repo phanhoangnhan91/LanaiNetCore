d700e22c055f4875a40232866ba3dfeb9757e49d
Nhan Phan <nhanphan@futurify.vn>
2016-07-05 16:33:23 +0700

    only load first time

--- ClientApp/components/home/home.ts
+++ ClientApp/components/home/home.ts
@@ -2,20 +2,26 @@ import * as ng from '@angular/core';
 import {POIService} from '../shared/pOI.service'
 import { Shared } from '../shared/shared';
 import {Loading} from "../shared/loading.component"
-
+var $ = require('jquery');
 @ng.Component({
   selector: 'home',
   template: require('./home.html'),
   providers: [POIService],
   directives: [Loading]
 })
-export class Home implements ng.AfterViewInit {
+export class Home implements ng.OnInit {
     constructor(private pOIService: POIService, private shared: Shared) {
        
     }
 
-    ngAfterViewInit() {       
-      // this.pOIService.getPOIsData();
+    ngOnInit() {       
+        var $this = this;
+        $("#container").ready(function () { // make it client site load
+            if ($this.shared.pOIs.length == 0)
+            {
+                $this.pOIService.getPOIsData();
+            }
+        });
     } 
    
     loadMore() {
